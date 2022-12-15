ffc78d1dc51f6f6023af3e3115464947703b59f9
omatheuslima <matheusl@comprovei.com>
2022-12-15 15:53:01 -0300

    release 02

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { getManager } from "typeorm";
+import { AdminController } from "../controllers/AdminController";
 import { AuthController } from "../controllers/AuthController";
+import { ManagerController } from "../controllers/ManagersController";
 import { OrderController } from "../controllers/OrderController";
 import { UserController } from "../controllers/UsersController";
 import { connect } from "../database/index";
@@ -27,6 +29,20 @@ routes.route("/users").get(new UserController().getUsers);
 routes.route("/user").post(new UserController().createUser);
 routes.route('/user/:id').delete(new UserController().deleteUser);
 routes.route('/user/:id').patch(new UserController().editUser);
+///////////////////////////////////////////////////////////////////////
+
+/////////////// CRUD DE GERENTES
+routes.route("/managers").get(new ManagerController().getManagers);
+routes.route("/manager").post(new ManagerController().createManager);
+routes.route('/manager/:id').delete(new ManagerController().deleteManager);
+routes.route('/manager/:id').patch(new ManagerController().editManager);
+///////////////////////////////////////////////////////////////////////
+
+/////////////// CRUD DE ADMINS
+routes.route("/admins").get(new AdminController().getAdmins);
+routes.route("/admin").post(new AdminController().createAdmin);
+routes.route('/admin/:id').delete(new AdminController().deleteAdmin);
+routes.route('/admin/:id').patch(new AdminController().editAdmin);
 ///////////////////////////////////////////////////////////////////////
 
 export { routes };
